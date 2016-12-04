5f3a375ebbb76ed6316eaf1e88c4aec63d13a79a
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-04 12:39:50 +0000

    writing block calculator

---
+++ lib/day_1.py
@@ -0,0 +1,10 @@
+class BlockCalculator(object):
+
+    def run(self, input):
+        instructions = input.split(', ')
+        start = [0, 0]
+        for instruction in instructions:
+            direction = instruction[0]
+            distance = instruction[1]
+
+        return input
