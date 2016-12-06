62768468db700a503751ba1643820b42fd888183
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-06 22:58:58 +0000

    Set up test for day 2 - code decoder

---
+++ tests/test_day_2.py
@@ -0,0 +1,17 @@
+import unittest
+from lib.day_2 import Decoder
+from tests.input.input_day_2 import bathroom_code_instructions
+
+class Decoder(unittest.TestCase):
+    def setUp(self):
+        self.decoder = Decoder()
+        self.puzzle_input = bathroom_code_input
+
+    def test_decodes_instructions(self):
+        instructions = [
+            'ULL',
+            'RRDDD',
+            'LURDL',
+            'UUUUD'
+        ]
+        self.assertEqual(self.decoder.run(instructions), '1985')
