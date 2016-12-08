3f09185ecff6740cd7b367c33021911bde182a28
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-08 23:04:04 +0000

    transposing array

--- lib/day_3.py
+++ lib/day_3.py
@@ -1,12 +1,24 @@
 class TriangleFinder(object):
     def count(self, side_lengths):
-        sets = side_lengths.split('\n')
+        sets = self._split_lines(side_lengths)
         tally = 0
         for set in sets:
-            sides = list(map(int, set.strip().split()))
+            sides = list(map(int, self._get_sides(set)))
             if self._is_triangle(sides):
                 tally += 1
         return tally
+
+    def count_by_column(self, side_lengths):
+        lines = self._split_lines(side_lengths)
+        horizonal_list = list(map(self._get_sides, lines))
+        vertial_list = list(map(list, zip(*horizonal_list)))
+        print(vertial_list)
+
+    def _split_lines(self, side_lengths):
+        return side_lengths.split('\n')
+
+    def _get_sides(self, set):
+        return set.strip().split()
 
     def _is_triangle(self, sides):
         if len(sides) == 3:
