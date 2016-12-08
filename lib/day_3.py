042a360c47b233bf9149721b5274505322247db7
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-08 22:30:17 +0000

    Refactored day 3 solution

--- lib/day_3.py
+++ lib/day_3.py
@@ -4,10 +4,13 @@ class TriangleFinder(object):
         tally = 0
         for set in sets:
             sides = list(map(int, set.strip().split()))
-            if len(sides) == 3:
-                x = sides[0]
-                y = sides[1]
-                z = sides[2]
-                if x + y > z and y + z > x and x + z > y:
-                    tally += 1
+            if self._is_triangle(sides):
+                tally += 1
         return tally
+
+    def _is_triangle(self, sides):
+        if len(sides) == 3:
+            x = sides[0]
+            y = sides[1]
+            z = sides[2]
+            return x + y > z and y + z > x and x + z > y
