4f7c088aecf7eac9b77cd7422c4a64d725fbe2c1
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-08 21:15:15 +0000

    wrote test for day 3 triangle finder

--- tests/test_day_3.py
+++ tests/test_day_3.py
@@ -4,4 +4,8 @@ from lib.day_3 import TriangleFinder
 class TriangleFinderTestCase(unittest.TestCase):
     def setUp(self):
         self.triangle_finder = TriangleFinder
-        self.side_lengths = '5 10 25'
+
+    def test_finds_number_of_triangles(self):
+        side_lengths = '5 10 25'
+        self.assertEqual(self.triangle_finder.count(side_length))
+        self.assertEqual(self.triangle_finder.tally, 1)
