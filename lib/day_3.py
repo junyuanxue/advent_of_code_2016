f174661bbbd7a3dc87e82cc5bd6be6030bd361a2
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-10 22:20:41 +0000

    refactored

--- lib/day_3.py
+++ lib/day_3.py
@@ -11,13 +11,8 @@ class TriangleFinder(object):
     def count_by_column(self, side_lengths):
         lines = self._split_lines(side_lengths)
         horizonal_list = list(map(self._get_sides, lines))
-        vertical_list = [[], [], []]
-        for line in horizonal_list:
-            for index, length in enumerate(line):
-                vertical_list[index].append(line[index])
-
-        print(vertical_list)
-        sets = self._get_sets_of_side_lengths(vertial_list)
+        columns = self._get_columns(horizonal_list)
+        sets = self._get_sets_of_side_lengths(columns)
 
         tally = 0
         for set in sets:
@@ -40,14 +35,21 @@ class TriangleFinder(object):
             z = sides[2]
             return x + y > z and y + z > x and x + z > y
 
+    def _get_columns(self, horizonal_list):
+        columns = [[], [], []]
+        for line in horizonal_list:
+            for index, length in enumerate(line):
+                columns[index].append(line[index])
+        return columns
+
     def _slice(self, long_list, n):
         for i in range(0, len(long_list), n):
             yield long_list[i:i + n]
 
-    def _get_sets_of_side_lengths(self, vertial_list):
+    def _get_sets_of_side_lengths(self, columns):
         sets = []
-        for long_list in vertial_list:
-            short_lists = list(self._slice(long_list, 3))
+        for column in columns:
+            short_lists = list(self._slice(column, 3))
             for short_list in short_lists:
                 sets.append(short_list)
         return sets
