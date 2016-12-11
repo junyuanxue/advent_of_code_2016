87c5d5f8f7284858c0da80be35e34c8ef95ec703
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-11 17:30:56 +0000

    wrote solution which is incredibly slow...

--- tests/test_day_5.py
+++ tests/test_day_5.py
@@ -6,8 +6,8 @@ class PasswordFinderTestCase(unittest.TestCase):
         self.password_finder = PasswordFinder()
 
     def test_finds_password(self):
-        password = password_finder.run('abc')
+        password = self.password_finder.run('abc')
         self.assertEqual(password, '18f47a30')
 
-        puzzle_door_id = 'cxdnnyjw'
-        puzzle_password = password_finder.run(puzzle_door_id)
+        door_id = 'cxdnnyjw'
+        puzzle_password = self.password_finder.run(door_id)
