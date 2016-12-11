626e3c8ed50c56977330768ea89ecf485922f081
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-11 17:44:57 +0000

    test green

--- lib/day_5.py
+++ lib/day_5.py
@@ -4,11 +4,10 @@ class PasswordFinder(object):
     def run(self, door_id):
         password = ''
         index = 0
-        while len(password) < len(door_id):
+        while len(password) < 8:
             data = door_id + str(index)
             code = hashlib.md5(data.encode('utf-8')).hexdigest()
-            print(code)
-            if (code[0:3] == '00000'):
+            if (code[0:5] == '00000'):
                 password += code[5]
             index += 1
         return password
--- tests/test_day_5.py
+++ tests/test_day_5.py
@@ -9,5 +9,5 @@ class PasswordFinderTestCase(unittest.TestCase):
         password = self.password_finder.run('abc')
         self.assertEqual(password, '18f47a30')
 
-        door_id = 'cxdnnyjw'
-        puzzle_password = self.password_finder.run(door_id)
+        # door_id = 'cxdnnyjw'
+        # puzzle_password = self.password_finder.run(door_id)
