5a54eed9b7cbcf873013c9dfda60e7e7c60391d8
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-11 01:02:30 +0000

    getting an array of highest values. answer still not correct :(

--- lib/day_4.py
+++ lib/day_4.py
@@ -11,8 +11,10 @@ class RoomDecoder(object):
             first_part = room.split('[')[0]
             sector_id = first_part.split('-').pop()
             letters = re.sub('[^a-zA-Z]', '', first_part)
+
             frequencies = self._map_letter_frequencies(list(letters))
-            largest_values = sorted(frequencies.values(), reverse=True)[:5]
+            sorted_frequencies = sorted(frequencies.values(), reverse=True)
+            largest_values = self._get_largest_values(sorted_frequencies, 5)
             if self._is_real_room(checksum, frequencies, largest_values):
                 self.sum_of_sector_ids += int(sector_id)
 
@@ -22,6 +24,13 @@ class RoomDecoder(object):
             frequency = frequencies[letter] + 1 if letter in frequencies else 1
             frequencies[letter] = frequency
         return frequencies
+
+    def _get_largest_values(self, numbers, length):
+        values = []
+        for number in numbers:
+            if number not in values and len(values) <= 5:
+                values.append(number)
+        return values
 
     def _is_real_room(self, checksum, frequencies, largest_values):
         for letter in checksum:
