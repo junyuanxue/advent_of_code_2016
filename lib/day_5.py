f0e741953dad851f91ae39006ccb05e3fbaa4eed
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-11 18:10:02 +0000

    method done

--- lib/day_5.py
+++ lib/day_5.py
@@ -1,24 +1,27 @@
 import hashlib
+import re
 
 class PasswordFinder(object):
     def run_simple_mode(self, door_id):
         password = ''
-        index = 0
+        counter = 0
         while len(password) < 8:
-            data = door_id + str(index)
+            data = door_id + str(counter)
             code = hashlib.md5(data.encode('utf-8')).hexdigest()
-            if (code[0:5] == '00000'):
+            if code[0:5] == '00000':
                 password += code[5]
-            index += 1
+            counter += 1
         return password
 
     def run_advanced_mode(self, door_id):
-        password = ''
-        index = 0
-        while len(password) < 8:
-            data = door_id + str(index)
+        password = list('????????')
+        counter = 0
+        while '?' in password:
+            data = door_id + str(counter)
             code = hashlib.md5(data.encode('utf-8')).hexdigest()
-            if (code[0:5] == '00000'):
-                password += code[5]
-            index += 1
-        return password
+            if code[0:5] == '00000' and re.search('[0-7]', code[5]):
+                index = code[5]
+                del password[index]
+                password.insert(index, code[6])
+            counter += 1
+        return ''.join(password)
