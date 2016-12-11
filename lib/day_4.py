d220f7cbdd3b345c39e00d35e4f50861d1c90156
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-11 00:45:38 +0000

    tests green!

--- lib/day_4.py
+++ lib/day_4.py
@@ -1,5 +1,4 @@
 import re
-import operator
 
 class RoomDecoder(object):
     def __init__(self):
@@ -12,21 +11,20 @@ class RoomDecoder(object):
             first_part = room.split('[')[0]
             sector_id = first_part.split('-').pop()
             letters = re.sub('[^a-zA-Z]', '', first_part)
-            print(checksum)
-            print(sector_id)
-            print(letters)
-            most_common_letters = self._get_most_common_letters(letters, 5)
-
-    def _get_most_common_letters(self, letters, number):
-        letters = list(letters)
-        frequencies = self._map_letter_frequencies(letters)
-        print(frequencies)
+            frequencies = self._map_letter_frequencies(list(letters))
+            largest_values = sorted(frequencies.values(), reverse=True)[:5]
+            if self._is_real_room(checksum, frequencies, largest_values):
+                self.sum_of_sector_ids += int(sector_id)
 
     def _map_letter_frequencies(self, letters_list):
         frequencies = {}
         for letter in letters_list:
-            frequency = 1
-            if letter in frequencies:
-                frequency = frequencies[letter] + 1
+            frequency = frequencies[letter] + 1 if letter in frequencies else 1
             frequencies[letter] = frequency
         return frequencies
+
+    def _is_real_room(self, checksum, frequencies, largest_values):
+        for letter in checksum:
+            if letter not in frequencies or frequencies[letter] not in largest_values:
+                return False
+        return True
