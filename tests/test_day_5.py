c545f564790e6f036158a15bc1bfa3a636e5914e
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-11 18:35:13 +0000

    wrote test for day 6

--- tests/test_day_5.py
+++ tests/test_day_5.py
@@ -6,6 +6,7 @@ class PasswordFinderTestCase(unittest.TestCase):
         self.password_finder = PasswordFinder()
         self.door_id = 'cxdnnyjw'
 
+    @unittest.skip('skipping due to long runtime')
     def test_finds_password_in_simple_mode(self):
         password = self.password_finder.run_simple_mode('abc')
         self.assertEqual(password, '18f47a30')
@@ -13,6 +14,7 @@ class PasswordFinderTestCase(unittest.TestCase):
         puzzle_password = self.password_finder.run_simple_mode(self.door_id)
         self.assertEqual('f77a0e6e', puzzle_password)
 
+    @unittest.skip('skipping due to long runtime')
     def test_finds_password_in_advanced_mode(self):
         password = self.password_finder.run_advanced_mode('abc')
         self.assertEqual(password, '05ace8e3')
---
+++ tests/test_day_6.py
@@ -0,0 +1,13 @@
+import unittest
+from lib.day_6 import Messenger
+
+class MessengerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.messenger = Messenger()
+        self.signal = 'eedadn\ndrvtee\neandsr\nraavrd\natevrs\ntsrnev\nsdttsa\nrasrtv\nnssdts\nntnada\nsvetve\ntesnvt\nvntsnd\nvrdear\ndvrsen\nenarar'
+        with open('tests/input/input_day_6.txt', 'r') as input_file:
+            self.puzzle_signal = input_file.read()
+
+    def test_reads_message_from_signal(self):
+        message = self.messenger.read(signal)
+        self.assertEqual(message, 'easter')
