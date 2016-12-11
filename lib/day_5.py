562da53a8b07e59ef8f5351e4e2d1ddb66041db5
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-11 17:55:13 +0000

    test for 2nd part...

--- lib/day_5.py
+++ lib/day_5.py
@@ -1,7 +1,18 @@
 import hashlib
 
 class PasswordFinder(object):
-    def run(self, door_id):
+    def run_simple_mode(self, door_id):
+        password = ''
+        index = 0
+        while len(password) < 8:
+            data = door_id + str(index)
+            code = hashlib.md5(data.encode('utf-8')).hexdigest()
+            if (code[0:5] == '00000'):
+                password += code[5]
+            index += 1
+        return password
+
+    def run_advanced_mode(self, door_id):
         password = ''
         index = 0
         while len(password) < 8:
--- tests/test_day_5.py
+++ tests/test_day_5.py
@@ -5,10 +5,14 @@ class PasswordFinderTestCase(unittest.TestCase):
     def setUp(self):
         self.password_finder = PasswordFinder()
 
-    def test_finds_password(self):
-        password = self.password_finder.run('abc')
-        self.assertEqual(password, '18f47a30')
+    # def test_finds_password_in_simple_mode(self):
+    #     password = self.password_finder.run_simple_mode('abc')
+    #     self.assertEqual(password, '18f47a30')
+    #
+    #     door_id = 'cxdnnyjw'
+    #     puzzle_password = self.password_finder.run_simple_mode(door_id)
+    #     self.assertEqual('f77a0e6e', puzzle_password)
 
-        door_id = 'cxdnnyjw'
-        puzzle_password = self.password_finder.run(door_id)
-        self.assertEqual('f77a0e6e', puzzle_password)
+    def test_finds_password_in_advanced_mode(self):
+        password = self.password_finder.run_advanced_mode('abc')
+        self.assertEqual(password, '05ace8e3')
