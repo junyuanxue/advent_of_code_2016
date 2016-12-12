04b542df7c00cfc0c41ce74a584e0967fd4cbd46
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-12 22:16:23 +0000

    Wrote test for day 7

---
+++ tests/test_day_7.py
@@ -0,0 +1,15 @@
+import unittest
+from lib.day_7 import IPChecker
+
+class IPCheckerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.ip_checker = IPChecker()
+        self.ips = 'abba[mnop]qrst\nabcd[bddb]xyyx\naaaa[qwer]tyui\nioxxoj[asdfgh]zxcvbn'
+        with open('tests/input/input_day_7.txt', 'r') as input_file:
+            self.puzzle_ips = input_file.read()
+
+    def test_counts_ips_that_support_tls(self):
+        number = self.ip_checker.count(self.ips)
+        self.assertEqual(number, 2)
+
+        
