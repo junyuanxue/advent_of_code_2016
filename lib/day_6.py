237fc5b08d91eb3415c165adc2fb1648598fb19a
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-12 21:38:22 +0000

    test passing

--- lib/day_6.py
+++ lib/day_6.py
@@ -1,10 +1,21 @@
+from collections import Counter
+
 class Messenger(object):
     def read(self, signal):
         lines = signal.strip().split('\n')
         columns = self._get_columns(lines)
-        return 'hello'
+        message = ''
+        for column in columns:
+            occurrences = dict(Counter(column))
+            letter = sorted(occurrences, key=occurrences.get, reverse=True)[0]
+            message += letter
+        return message
 
     def _get_columns(self, lines):
         columns = [[], [], [], [], [], []]
         for line in lines:
-            
+            index = 0
+            while index < len(line):
+                columns[index].append(line[index])
+                index += 1
+        return columns
