2344f493c98718b8447a3e24e6cf6ccc12335cb4
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-13 09:22:04 +0000

    test green for day 7!

--- lib/day_7.py
+++ lib/day_7.py
@@ -10,11 +10,16 @@ class IPChecker(object):
         return tally
 
     def _supports_tls(self, ip):
-
         normal_strings = re.findall(r'\]\w+$|^\w+\[|\]\w+\[', ip)
         hypernet_strings = re.findall(r'\[\w+\]', ip)
         return self._has_abba(normal_strings) and not self._has_abba(hypernet_strings)
 
     def _has_abba(self, strings):
         for string in strings:
-            print(string)
+            string = re.sub(r'[^a-zA-Z]+', '', string)
+            i = 0
+            while i <= len(string) - 4:
+                if string[i] != string[i + 1] and string[i + 1] == string[i + 2] and string[i + 2] != string[i + 3] and string[i] == string[i + 3]:
+                    return True
+                else:
+                    i += 1
