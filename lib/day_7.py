77bbdcef3b0176a60e63b77c2bb8914bac7e0cdf
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-13 21:47:37 +0000

    commit

--- lib/day_7.py
+++ lib/day_7.py
@@ -10,7 +10,6 @@ class IPChecker(object):
         return tally
 
     def _supports_tls(self, ip):
-
         normal_strings = re.findall(r'\]\w+$|^\w+\[|\]\w+\[', ip)
         hypernet_strings = re.findall(r'\[\w+\]', ip)
         return self._has_abba(normal_strings) and not self._has_abba(hypernet_strings)
