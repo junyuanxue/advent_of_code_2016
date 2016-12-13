1b14f65ac5dfb02cc48cd4f49a79d53233176e6e
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-13 21:56:45 +0000

    wrote test for second day

--- tests/test_day_7.py
+++ tests/test_day_7.py
@@ -5,6 +5,7 @@ class IPCheckerTestCase(unittest.TestCase):
     def setUp(self):
         self.ip_checker = IPChecker()
         self.ips = 'abba[mnop]qrst\nabcd[bddb]xyyx\naaaa[qwer]tyui\nioxxoj[asdfgh]zxcvbn'
+        self.more_ips = 'aba[bab]xyz\nxyx[xyx]xyx\naaa[kek]eke\nzazbz[bzb]cdb'
         with open('tests/input/input_day_7.txt', 'r') as input_file:
             self.puzzle_ips = input_file.read()
 
@@ -14,3 +15,9 @@ class IPCheckerTestCase(unittest.TestCase):
 
         puzzle_tally = self.ip_checker.count(self.puzzle_ips, 'tls')
         self.assertEqual(puzzle_tally, 110)
+
+    def test_counts_ips_that_support_ssl(self):
+        number = self.ip_checker.count(self.more_ips, 'ssl')
+        self.assertEqual(number, 3)
+
+        puzzle_tally = self.ip_checker.count(self.puzzle_ips, 'ssl')
