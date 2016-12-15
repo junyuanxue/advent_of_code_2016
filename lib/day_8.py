c08f8c4b7084867bb7b17bc11096f2cf76f2026c
Junyuan Xue <junyuanxue.is@gmail.com>
2016-12-15 23:14:55 +0000

    simplified switch method

--- lib/day_8.py
+++ lib/day_8.py
@@ -13,24 +13,30 @@ class Authy(object):
                 self._turn_on_rectangle(width, height)
                 print(self.screen)
             if 'rotate' in step:
-                distance = int(step.split(' ').pop())
                 coordinate = step.split('=')[0][-1]
-                position = step.split('=')[1][0]
+                position = int(step.split('=')[1][0])
+                distance = int(step.split(' ').pop())
+                self._rotate(coordinate, position, distance)
+        print(self.screen)
         return '1'
 
     def _turn_on_rectangle(self, width, height):
         for i, row in enumerate(self.screen):
             if i < height:
                 j = 0
-                while j <= width:
+                while j < width:
                     self._switch('on', i, j)
                     j += 1
 
     def _switch(self, mode, row_index, column_index):
-        row = self.screen[row_index]
-        string = '-'
-        replacement = '#'
-        if mode == 'off':
-            string = '#'
-            replacement = '-'
-        self.screen[row_index] = row[:column_index].replace(string, replacement) + row[column_index:]
+        row = list(self.screen[row_index])
+        value = '#' if mode == 'on' else '-'
+        row[column_index] = value
+        self.screen[row_index] = ''.join(row)
+
+    def _rotate(self, coordinate, position, distance):
+        if coordinate == 'x':
+            for i, row in enumerate(self.screen):
+                value = row[position]
+                new_index = i + distance
+                # self.screen[new_index] = row[:position].replace(, value) + row[position:]
